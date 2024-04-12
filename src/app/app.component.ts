554cc75db1c872c8e4d4635b5fec09335210ded6
phte1100 <phte1100@student.miun.se>
2024-04-12 09:04:15 +0200

    Tog bort live input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { LiveInputComponent } from './live-input/live-input.component';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, LiveInputComponent, FormsModule],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
