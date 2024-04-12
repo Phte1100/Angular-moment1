c182a9e5d4f3d03e4c63d75d573d81638e6b78a1
phte1100 <phte1100@student.miun.se>
2024-04-12 10:22:13 +0200

    ordnat med all konvertering nu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,10 +11,24 @@ import { FormsModule } from '@angular/forms';
 })
 export class HomeComponent {
   celsius: number | null = null; // sätter initialt värde till null
+  fahrenheit: number | null = null; // sätter initialt värde till null
+  meter: number | null = null; // sätter initialt värde till null
+  feet: number | null = null; // sätter initialt värde till null
 
   convertToFahrenheit(): number | null {
     // Endast konvertera om det finns ett giltigt värde
     return this.celsius !== null ? (this.celsius * 9 / 5) + 32 : null;
+  }
+  convertToCelsius(): number | null {
+    // Endast konvertera om det finns ett giltigt värde
+    // Endast 1 decimal
+    return this.fahrenheit !== null ? parseFloat(((this.fahrenheit - 32) * 5 / 9).toFixed(1)) : null;
+  }
+  convertToFeet(): number | null {
+    return this.meter !== null ? parseFloat(((this.meter * 3.28)).toFixed(1)) : null;
+  }
+  convertToMeter(): number | null {
+    return this.feet !== null ? parseFloat((this.feet * 0.3048).toFixed(1)) : null;
   }
 }
 
